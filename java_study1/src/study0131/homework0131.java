2896f2ad036f45e917b2606cbc9611d5f1422ff1
Myeongjun-98 <anju304@naver.com>
2025-02-02 20:55:31 +0900

    숙제완료

--- java_study1/src/study0131/homework0131.java
+++ java_study1/src/study0131/homework0131.java
@@ -17,8 +17,45 @@ public class homework0131 {
 		// 8시간 이상 주차시 무조건 10000원이다.
 		// 주차시간(몇시간 몇분) 입력하여 주차요금이 얼마인지 출력하세요.
 		
+		Scanner scan = new Scanner(System.in);
+		int defaultPrice = 1000;
+		int defaultPrice2h = 1700;
+		int defaultTime = 30;
+		int defaultTime2h = 120;
+		int defaultPrice4h = 2500;
+		int defaultTime4h = 240;
+		int defaultPrice8h = 10000;
+		int extraPrice = 100;
+		int totalPrice = 0;
+		int time = 0;
+		int 시간 = 0;
+		int 분 = 0;
+		System.out.println("주차시간을 입력하세요.");
+		System.out.print("주차시간 : □시간 __분 : ");
+		시간 = scan.nextInt();
+		System.out.print("주차시간 " + 시간 + "시간"  + " □□분  : ");
+		분 = scan.nextInt();
+		time = 시간 * 60 + 분;
+		
+		if (time >= 0 && time <= 39) 
+			{ totalPrice = defaultPrice;
+				} else if (time >= 40 && time <= 119) {
+					totalPrice = defaultPrice + ((time - defaultTime) / 10) * extraPrice ;
+					}else if (time >= 120 && time <= 129) {
+						totalPrice = defaultPrice2h;
+						}else if (time >= 130 && time <= 239) {
+							totalPrice = defaultPrice2h + ((time - defaultTime2h) / 10) * extraPrice ;
+								}else if (time >= 240 && time <= 249) {
+									totalPrice = defaultPrice4h;
+									}else if (time >= 250 && time <= 479) {
+										totalPrice = defaultPrice4h + ((time - defaultTime4h) / 10) * extraPrice ;
+										}else if (time >= 480) {
+											totalPrice = defaultPrice8h;}
+		System.out.println("당신의 요금은 " + totalPrice + "원 입니다.");
+			
 		
 		
+			
 		
 		
 		

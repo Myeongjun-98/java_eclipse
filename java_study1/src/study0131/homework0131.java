20fdad01bf0df9d436bdff69f6b1baf87d2484a5
Myeongjun-98 <anju304@naver.com>
2025-02-03 09:46:06 +0900

    0131숙제 + 해설본

--- java_study1/src/study0131/homework0131.java
+++ java_study1/src/study0131/homework0131.java
@@ -36,37 +36,52 @@ public class homework0131 {
 		System.out.print("주차시간 " + 시간 + "시간"  + " □□분  : ");
 		분 = scan.nextInt();
 		time = 시간 * 60 + 분;
-		
-/*		if (time >= 0 && time <= 39) 
-			{ totalPrice = defaultPrice;
-				} else if (time >= 40 && time <= 119) {
-					totalPrice = defaultPrice + ((time - defaultTime) / 10) * extraPrice ;
-					}else if (time >= 120 && time <= 129) {
-						totalPrice = defaultPrice2h;
-						}else if (time >= 130 && time <= 239) {
-							totalPrice = defaultPrice2h + ((time - defaultTime2h) / 10) * extraPrice ;
-								}else if (time >= 240 && time <= 249) {
-									totalPrice = defaultPrice4h;
-									}else if (time >= 250 && time <= 479) {
-										totalPrice = defaultPrice4h + ((time - defaultTime4h) / 10) * extraPrice ;
-										}else if (time >= 480) {
-											totalPrice = defaultPrice8h;}
-	*/	
+//	
 		if (time >= 0 && time <= 39) 
-		{ totalPrice = defaultPrice;
-			} else if (time <= 119) {
-				totalPrice = defaultPrice + ((time - defaultTime) / 10) * extraPrice ;
-				}else if (time <= 129) {
-					totalPrice = defaultPrice2h;
-					}else if (time <= 239) {
-						totalPrice = defaultPrice2h + ((time - defaultTime2h) / 10) * extraPrice ;
-							}else if (time <= 249) {
-								totalPrice = defaultPrice4h;
-								}else if (time <= 479) {
-									totalPrice = defaultPrice4h + ((time - defaultTime4h) / 10) * extraPrice ;
-									}else if (time >= 480) {
-										totalPrice = defaultPrice8h;}
+		{ 
+			totalPrice = defaultPrice;
+		} else if (time <= 119) {
+			totalPrice = defaultPrice + ((time - defaultTime) / 10) * extraPrice ;
+		}else if (time <= 129) {
+			totalPrice = defaultPrice2h;
+		}else if (time <= 239) {
+			totalPrice = defaultPrice2h + ((time - defaultTime2h) / 10) * extraPrice ;
+		}else if (time <= 249) {
+			totalPrice = defaultPrice4h;
+		}else if (time <= 479) {
+			totalPrice = defaultPrice4h + ((time - defaultTime4h) / 10) * extraPrice ;
+		}else if (time >= 480) {
+			totalPrice = defaultPrice8h;
+		}		
+
 		System.out.println("당신의 요금은 " + totalPrice + "원 입니다.");
+
+		//		해설본 (코드가 대량 단축됨)
+		
+//		int phour, pmin;	// 주차 시간, 분
+//		int cost = 1000;
+//		int totalTime = 0;	// 총 주차 시간(분)	=> 기본시간을 제외한 나머지
+//		
+//		System.out.println("주차 시간( 시간, 분) : ");
+//		phour = scan.nextInt();	// 주차 몇 시간 입력
+//		pmin = scan.nextInt();	// 주차 몇 분 입력
+//		
+//		totalTime = phour*60 + pmin;
+//		if(phour >= 8) {		// 8시간 이상 주차시
+//			cost = 10000;
+//			totalTime = 0;	// 8시간 이상이라는 시간에 대해 이미 요금계산 완료됨
+//		}else if (phour >= 4) {//4시간 이상 주차시
+//			cost = 2500;
+//			totalTime -= 240; 
+//		}else if (phour >= 2) {
+//			cost = 1700;
+//			totalTime -= 120;
+//		}else {
+//			totalTime -= 30;
+//		}
+//		
+//		int price = cost + (totalTime /10 * 100);
+//		System.out.println( "요금은 " + price + "원 입니다.");
 			
 		
 		

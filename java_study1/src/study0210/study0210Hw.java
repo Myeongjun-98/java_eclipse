72603134400586e33f6755844a0d7017b20aaa7a
Myeongjun-98 <anju304@naver.com>
2025-02-10 17:27:20 +0900

    0210 Array

---
+++ java_study1/src/study0210/study0210Hw.java
@@ -0,0 +1,29 @@
+package study0210;
+
+import java.util.Scanner;
+
+public class study0210Hw {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		// 랜덤범위 1 ~ 50
+		// 랜덤 숫자 중에서 짝수 15개를 배열에 저장
+		// 15개 전주 저장되면 배열 출력
+		// 숫자 중복 상관없음
+		// 중복 없이도 해볼까?
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ java_study1/src/study0210/study0211.java
@@ -0,0 +1,17 @@
+package study0210;
+
+public class study0211 {
+
+	public static void main(String[] args) {
+		
+		int[] num = new int[]	{10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+		
+		for(int a : num) {
+			System.out.println(a);
+		}
+			
+		
+			
+	}
+
+}

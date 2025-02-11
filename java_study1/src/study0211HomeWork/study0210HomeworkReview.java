9794b6c80c0290a6888d019ea3f43913d8df419f
Myeongjun-98 <anju304@naver.com>
2025-02-11 09:45:01 +0900

    중간

--- java_study1/src/study0211HomeWork/study0210HomeworkReview.java
+++ java_study1/src/study0211HomeWork/study0210HomeworkReview.java
@@ -3,44 +3,32 @@ package study0211HomeWork;
 public class study0210HomeworkReview {
 
 	public static void main(String[] args) {
-			
-		//해설
-		
+
+		// 해설
+
 		int[] num = new int[15];
-		
-		int idx=0;
-		while(true) {
-			int temp = (int)(Math.random()*50 + 1);
-			if( temp % 2 == 0 ) {	// 2(나머지)나누기 했을 때 나머지가 0이면 짝수
-				
-				
-				
-				
-				
-				
-				
-				
+
+		int idx = 0;
+		while (true) {
+			int temp = (int) (Math.random() * 50 + 1);
+			if (temp % 2 == 0) { // 2(나머지)나누기 했을 때 나머지가 0이면 짝수
+				boolean isSame = false;		// 같으면 true, 같지 않으면 false
+				for ( int i = 0; i < idx; i++) {
+					if(num[i] == temp) {
+						isSame = true;
+					}
+				}
 				num[idx++] = temp;
-				
-				
-				
-				
-				
-				
-				
-			}if(idx == num.length) break;
-			
-			
+
+			}
+			if (idx == num.length)
+				break;
+
 		}
-		for(int i = 0; i < num.length; i++) {
-		System.out.println(num[i]);
+		for (int i = 0; i < num.length; i++) {
+			System.out.println(num[i]);
 		}
-		
-		
-	
-		
-		
-		
+
 	}
 
 }
